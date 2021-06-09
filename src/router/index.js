4771244aa21a426c011a45e8f2af5df07abb7630
FariaSta <davidfarias30@gmail.com>
2021-06-09 15:16:05 -0400

    Error.vue and about.vue added

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,17 @@ const routes = [
     name: 'Home',
     component: () => import('../views/Home.vue')
   },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '*',
+    name: 'Error',
+    component: () => import('../views/Error.vue')
+  }
+
 ]
 
 const router = new VueRouter({
